fdf968c46dcc93a6ed1427aa9a02572b593f07f8
Weverton dos Santos <wever-santos@hotmail.com>
2023-02-27 18:03:10 -0300

    method catch

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
-const consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-.then(resposta => resposta.json())
-.then(r => console.log(r))
+const consultaCEP = fetch('https://viacep.com.br/ws/01001252/json/')
+    .then(resposta => resposta.json())
+    .then(r => {
+        if(r.erro){
+            throw Error ('Esse CEP não existe')
+        }else{
+            console.log(r)
+        }
+    })
+    .catch(erro => {
+        if(erro){
+            console.log('Tente outro CEP: tem que ter {8} dígitos, por exemplo: 01001000')
+        }
+    })
