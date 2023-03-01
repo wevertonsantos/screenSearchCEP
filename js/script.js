54cc911f6641f24f2d6d569f94d2184e928081d3
Weverton dos Santos <wever-santos@hotmail.com>
2023-03-01 18:29:55 -0300

    request VIACEP API sucess

--- js/script.js
+++ js/script.js
@@ -1,12 +1,46 @@
+    //  Button of search
     const btnSearch = document.querySelector('#submit-cep')
 
+    // Event click on button of search
     btnSearch.addEventListener('click', e => {
         e.preventDefault()
-        console.log(this)
-    })
 
-    const url = fetch('http://viacep.com.br/ws/01001000/json/')
-    .then(response => response.json())
-    .then(r => console.log(r))
+        // Get input if element CEP
+        const inputCep = document.querySelector('#cep')
 
-    console.log(url)
+        // Get value of element CEP
+        const valueCep = inputCep.value
+
+        // URL for fetch
+        const url = `https://viacep.com.br/ws/${valueCep}/json/`
+
+        // Request and fuction for insert data in elements
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+
+                    if (data.erro){
+                        alert('CEP INVALID')
+                        return;
+                    }
+
+                    insertDatas(data)
+                })
+
+        })
+
+    // Function for insert datas in elements
+
+        function insertDatas(data){
+
+            // Get elements for insert datas
+
+            const street = document.querySelector('#street')
+            const city = document.querySelector('#city')
+            const state = document.querySelector('#uf')
+
+            street.value = data.logradouro
+            city.value = data.localidade
+            state.value = data.uf
+
+        }
