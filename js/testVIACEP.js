c382175c9db61b20dbee1b1224095a1c53648c48
Weverton dos Santos <wever-santos@hotmail.com>
2023-03-02 12:40:59 -0300

    learning async await

--- js/testVIACEP.js
+++ js/testVIACEP.js
@@ -1,14 +1,29 @@
-const consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-    .then(resposta => resposta.json())
-    .then(r => {
-        if(r.erro){
-            throw Error ('')
-        }else{
-            console.log(r)
-        }
-    })
-    .catch(erro => {
-        if(erro){
-        }
-    })
-    .finally(mensagem => console.log())
+	
+	// request, return datas and treating
+
+	const consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
+	    .then(resposta => resposta.json())
+	    .then(r => {
+	        if(r.erro){
+	            throw Error ('')
+	        }else{
+	            console.log(r)
+	        }
+	    })
+	    .catch(erro => {
+	        if(erro){
+	        }
+	    })
+	    .finally(mensagem => console.log())
+
+    // async await
+
+	async function buscaEndereco(){
+		const consultaAsync = await fetch('https://viacep.com.br/ws/01001000/json/')
+		const consultaConvert = await consultaAsync.json();
+
+		console.log(consultaConvert);
+	}
+
+buscaEndereco()
+
