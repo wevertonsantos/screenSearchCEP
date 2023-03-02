d05fcc2943ab807b373a8675fda3f631a7ca5782
Weverton dos Santos <wever-santos@hotmail.com>
2023-03-02 12:23:58 -0300

    test viaCEP

--- js/testVIACEP.js
+++ js/testVIACEP.js
@@ -1,14 +1,14 @@
-// const consultaCEP = fetch('https://viacep.com.br/ws/01001252/json/')
-//     .then(resposta => resposta.json())
-//     .then(r => {
-//         if(r.erro){
-//             throw Error ('')
-//         }else{
-//             console.log(r)
-//         }
-//     })
-//     .catch(erro => {
-//         if(erro){
-//         }
-//     })
-//     .finally(mensagem => console.log())
+const consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
+    .then(resposta => resposta.json())
+    .then(r => {
+        if(r.erro){
+            throw Error ('')
+        }else{
+            console.log(r)
+        }
+    })
+    .catch(erro => {
+        if(erro){
+        }
+    })
+    .finally(mensagem => console.log())
